61b98e19319db4e0e8e747c712034a1abe83a3ed
YEONGCHANG YUN <94744740+YeongT@users.noreply.github.com>
2024-08-03 22:51:05 +0900

    🩹 Fix(Response): Specify return type of response utility methods
    - Updated response utility methods to specify return types.
    - Ensured all relevant methods are correctly typed.
    
    Signed-off-by: YEONGCHANG YUN <94744740+YeongT@users.noreply.github.com>

--- src/main/java/com/kanduit/sso/utils/response/APIResponseUtil.java
+++ src/main/java/com/kanduit/sso/utils/response/APIResponseUtil.java
@@ -4,7 +4,7 @@ import com.kanduit.sso.dto.response.BaseResponseDTO;
 import org.springframework.http.ResponseEntity;
 
 public abstract class APIResponseUtil {
-    public static <D, E> ResponseEntity<?> createResponse(BaseResponseDTO<D, E> body) {
+    public static <D, E> ResponseEntity<BaseResponseDTO<?, ?>> createResponse(BaseResponseDTO<D, E> body) {
         return new ResponseEntity<>(body, body.getStatus().getHttpStatus());
     }
 }
