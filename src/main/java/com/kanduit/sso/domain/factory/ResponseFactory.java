1f7b73925ada0eb0d252c1bc895086ab91e145ca
YEONGCHANG YUN <94744740+YeongT@users.noreply.github.com>
2024-08-15 03:48:42 +0900

    ✨ Feat(Response): Add endpoint information to response
    - Introduced `endpoint` field to include the request URI in the response.
    - Modified constructor to accept `WebRequest` and extract the request URI.
    - Updated `BaseResponseDTO` to include endpoint information alongside status, data, and error.
    - Updated `ResponseFactory` methods to include `WebRequest` for generating responses with endpoint information.
    
    This change helps in tracing and debugging by providing the URI of the request in the response.
    
    Signed-off-by: YEONGCHANG YUN <94744740+YeongT@users.noreply.github.com>

--- src/main/java/com/kanduit/sso/domain/factory/ResponseFactory.java
+++ src/main/java/com/kanduit/sso/domain/factory/ResponseFactory.java
@@ -4,27 +4,28 @@ import com.kanduit.sso.dto.response.APIResponseStatus;
 import com.kanduit.sso.dto.response.BaseResponseDTO;
 import lombok.NonNull;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.WebRequest;
 
 @Component
 public class ResponseFactory {
     // Create a simple response body with given status
-    public BaseResponseDTO<Void, Void> createSimpleStatusBody(@NonNull APIResponseStatus status) {
-        return createCustomBody(status, null, null);
+    public BaseResponseDTO<Void, Void> createSimpleStatusBody(@NonNull WebRequest request, @NonNull APIResponseStatus status) {
+        return createCustomBody(request, status, null, null);
     }
 
     // Create a response body with given status and data
-    public <D> BaseResponseDTO<D, Void> createDataBody(@NonNull APIResponseStatus status, @NonNull D data) {
-        return createCustomBody(status, data, null);
+    public <D> BaseResponseDTO<D, Void> createDataBody(@NonNull WebRequest request, @NonNull APIResponseStatus status, @NonNull D data) {
+        return createCustomBody(request, status, data, null);
     }
 
     // Create a response body with given status and error
-    public <E> BaseResponseDTO<Void, E> createErrorBody(@NonNull APIResponseStatus status, @NonNull E error) {
-        return createCustomBody(status, null, error);
+    public <E> BaseResponseDTO<Void, E> createErrorBody(@NonNull WebRequest request, @NonNull APIResponseStatus status, @NonNull E error) {
+        return createCustomBody(request, status, null, error);
     }
 
     // Create a response body with custom option
-    public <D, E> BaseResponseDTO<D, E> createCustomBody(@NonNull APIResponseStatus status, D data, E error) {
-        return new BaseResponseDTO<D, E>(status) {
+    public <D, E> BaseResponseDTO<D, E> createCustomBody(@NonNull WebRequest request, @NonNull APIResponseStatus status, D data, E error) {
+        return new BaseResponseDTO<D, E>(request, status) {
             public BaseResponseDTO<D, E> create(D data, E error) {
                 this.setData(data);
                 this.setError(error);
--- src/main/java/com/kanduit/sso/dto/response/BaseResponseDTO.java
+++ src/main/java/com/kanduit/sso/dto/response/BaseResponseDTO.java
@@ -5,18 +5,24 @@ import lombok.Getter;
 import lombok.NonNull;
 import lombok.Setter;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.context.request.WebRequest;
 
 @Getter
 @Setter
 @JsonInclude
 public abstract class BaseResponseDTO<D, E> {
     @NonNull
+    private final String endpoint;
+
+    @NonNull
     private final Status status;
 
     private D data;
     private E error;
 
-    public BaseResponseDTO(@NonNull APIResponseStatus responseStatus) {
+    public BaseResponseDTO(@NonNull WebRequest request, @NonNull APIResponseStatus responseStatus) {
+        this.endpoint = ((ServletWebRequest) request).getRequest().getRequestURI();
         this.status = new Status(responseStatus.getHttpStatus(), responseStatus.getHttpStatus().value(), responseStatus.getMessage());
     }
 
