dc8e461fd56e29e41d8cdbda5da21bd636c371f9
YEONGCHANG YUN <94744740+YeongT@users.noreply.github.com>
2024-08-17 15:25:00 +0900

    ✨ Feat(Response): Implement new method `createSimpleErrorResponse`
    - Introduced a new `createSimpleErrorResponse` method in `APIResponseUtil` to streamline error response creation.
    - The new method accepts `APIExceptionFactory`, `APIResponseStatus`, and `ArrayList<String> comments` to internally create an `APIException` and its body, simplifying the process of generating error responses.
    - This method reduces the verbosity of calling `createErrorBody` directly with a new `APIException`, providing a more convenient and readable way to handle error responses.
    - Existing methods using `APIExceptionBody` as a parameter are still supported, allowing flexibility in error response creation.
    
    No changes to existing functionality; the new method is provided as an additional convenience.
    
    Signed-off-by: YEONGCHANG YUN <94744740+YeongT@users.noreply.github.com>

--- src/main/java/com/kanduit/sso/utils/response/APIResponseUtil.java
+++ src/main/java/com/kanduit/sso/utils/response/APIResponseUtil.java
@@ -1,8 +1,10 @@
 package com.kanduit.sso.utils.response;
 
 import com.kanduit.sso.domain.factory.ResponseFactory;
+import com.kanduit.sso.dto.response.APIResponseStatus;
 import com.kanduit.sso.dto.response.StandardResponseDTO;
 import com.kanduit.sso.exception.APIExceptionBody;
+import com.kanduit.sso.exception.APIExceptionFactory;
 import lombok.NonNull;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -13,8 +15,12 @@ public class APIResponseUtil {
     @NonNull
     private final ResponseFactory responseFactory;
 
-    public APIResponseUtil(@NonNull ResponseFactory responseFactory) {
+    @NonNull
+    private final APIExceptionFactory exceptionFactory;
+
+    public APIResponseUtil(@NonNull ResponseFactory responseFactory, @NonNull APIExceptionFactory exceptionFactory) {
         this.responseFactory = responseFactory;
+        this.exceptionFactory = exceptionFactory;
     }
 
     public <D, E> ResponseEntity<StandardResponseDTO<D, E>> createResponse(@NonNull StandardResponseDTO<D, E> body) {
@@ -23,6 +29,13 @@ public class APIResponseUtil {
 
     public ResponseEntity<StandardResponseDTO<Void, APIExceptionBody>> createErrorResponse(@NonNull WebRequest request, @NonNull APIExceptionBody exceptionBody) {
         return createResponse(responseFactory.createErrorBody(request, exceptionBody.getResponseStatus(), exceptionBody));
+    }
+
+    public ResponseEntity<StandardResponseDTO<Void, APIExceptionBody>> createSimpleErrorResponse(
+            @NonNull WebRequest request,
+            @NonNull ResponseFactory responseFactory,
+            @NonNull APIResponseStatus responseStatus) {
+        return createResponse(responseFactory.createErrorBody(request, responseStatus, exceptionFactory.createException(responseStatus).getBody()));
     }
 }
 
