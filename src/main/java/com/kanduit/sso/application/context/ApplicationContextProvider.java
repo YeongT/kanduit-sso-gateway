d466d15ce7be893ae1e8f7fe6b54be35e7756e1f
YEONGCHANG YUN <94744740+YeongT@users.noreply.github.com>
2024-08-23 14:12:00 +0900

    ✨ Feat: Implement core classes for `FaviconService` functionality
    - Added `ApplicationContextProvider` for bean management in the Spring context.
    - Created `FaviconService` to handle favicon loading and Base64 encoding.
    - Introduced `Favicon` and `Extension` enums for managing file metadata and supported image types.
    
    These changes introduce a new service for favicon handling and improve email template customization with dynamic image insertion.
    
    Signed-off-by: YEONGCHANG YUN <94744740+yeongt@users.noreply.github.com>

---
+++ src/main/java/com/kanduit/sso/application/context/ApplicationContextProvider.java
@@ -0,0 +1,21 @@
+package com.kanduit.sso.application.context;
+
+import io.micrometer.common.lang.NonNull;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApplicationContextProvider implements ApplicationContextAware {
+    private static ApplicationContext applicationContext;
+
+    public static <T> T getBean(Class<T> beanClass) throws NoSuchBeanDefinitionException {
+        return applicationContext.getBean(beanClass);
+    }
+
+    @Override
+    public void setApplicationContext(@NonNull ApplicationContext applicationContext) {
+        ApplicationContextProvider.applicationContext = applicationContext;
+    }
+}
